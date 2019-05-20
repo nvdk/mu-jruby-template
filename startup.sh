d3999396cf83c31485c870576d53d966c86b138e
Niels Vandekeybus <progster@gmail.com>
2019-05-20 18:09:36 +0200

    optimize startup

--- startup.sh
+++ startup.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-gem install sinatra
-if [ "$RACK_ENV" == "production" ]; 
-then 
+if [ "$RACK_ENV" == "production" ];
+then
     bundle install --without development test
-    ruby $MAIN_APP_FILE -p 80
+    bundle exec ruby --server $MAIN_APP_FILE -p 80
 else
     bundle install
-    if [ "$RACK_ENV" == "test" ]; 
+    if [ "$RACK_ENV" == "test" ];
     then
-        rspec
+        bundle exec rspec
     else
-        gem install sinatra-contrib
-        ruby $MAIN_APP_FILE -p 80 -o '0.0.0.0'
+        bundle exec ruby --dev $MAIN_APP_FILE -p 80 -o '0.0.0.0'
     fi
 fi
